33c93d908713a97f014f5ac732fd902c4fe443d5
vchamala <49884971+vchamala@users.noreply.github.com>
2023-12-02 09:15:27 -0800

    Create extra_long_factorials.java

---
+++ hackerrank/extra_long_factorials.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+    // Write your code here
+        BigInteger sol = factorial(BigInteger.valueOf(n));
+        System.out.println(sol);
+    }
+    
+    // private static long fact(int n) {
+    //     if (n>1) {
+    //         return n * fact(n-1);
+    //     } else {
+    //         return 1;
+    //     }
+    // }
+    
+    public static BigInteger factorial(BigInteger number) {
+        if(number.equals(BigInteger.ONE)) {
+            return BigInteger.valueOf(1);
+        } else {
+            return number.multiply(factorial(number.subtract(BigInteger.ONE)));
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
