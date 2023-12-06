1d923dc42bb67b1b6196374f88614f249d940ea4
vchamala <49884971+vchamala@users.noreply.github.com>
2023-12-05 18:16:20 -0800

    Update minimum_size_subarray_sum.java

--- Sliding_Window/minimum_size_subarray_sum.java
+++ Sliding_Window/minimum_size_subarray_sum.java
@@ -1,37 +1,28 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        
-        // TODO - Please see: There is a Bug - Need to update for accurate solution.
-        
-        // sliding window x and y
-        
-        // keep moving y - when it goes past target - increment x 
+    public int minSubArrayLen(int target, int[] nums) {        
+        // sliding window i and j
+        // keep moving y - when it goes past target - increment i 
         // take the minimum of the subArray lengths which 
         // satisfied the condition..
-        
         int i=0;
-        int j=1;
-        int minLength = 1;
-        Queue<Integer> subArrayQueue = new LinkedList<Integer>();
-        subArrayQueue.add(nums[0]);
-        int currentCount = nums[0];
-        
-        while(i<j && j<nums.length) {
-            if (currentCount >= target) {
-                minLength = Math.min(minLength, subArrayQueue.size());
+        int j=0;
+        int currentCount=0;
+        int ans = Integer.MAX_VALUE;
+        while(j<nums.length) {
+            currentCount = currentCount + nums[j];
+            while (currentCount >= target) {
+                int len = j-i+1;
+                if (len < ans) {
+                    ans = len;
+                }
                 currentCount = currentCount - nums[i];
-                // removes the first inserted - FIFO.
-                subArrayQueue.remove();
                 i++;
-                continue;
-            } else {
-                currentCount = currentCount + nums[j];
-                // Inserts at the end of queue.
-                subArrayQueue.add(nums[j]);
-                minLength = subArrayQueue.size();
-                j++;
-            }  
+            } 
+            j++;
         }
-        return minLength;
+        if(ans == Integer.MAX_VALUE){
+            ans = 0;
+        }
+        return ans;
     }
 }
