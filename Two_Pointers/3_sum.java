ce36fcfe92f18a81c03688750fc1f6296a651cc5
vchamala <49884971+vchamala@users.noreply.github.com>
2023-12-08 21:05:11 -0800

    Update 3_sum.java

--- Two_Pointers/3_sum.java
+++ Two_Pointers/3_sum.java
@@ -5,13 +5,15 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         
         for (int i=0; i<len-2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             int l = i+1;
             int r = len-1;
             
             while(l<r) {
                 int sum = nums[i] + nums[l] + nums[r];
-
-                // for 3 sum to K - need to update this to K value
                 if(sum==0) {
                 // add these 3 elems..
                     List<Integer> num = new ArrayList<>();
@@ -19,6 +21,20 @@ class Solution {
                     num.add(nums[l]);
                     num.add(nums[r]);
                     res.add(num);
+                    
+                    // Skip duplicate elements for j
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+
+                    // Skip duplicate elements for k
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+
+                    // Move the pointers
+                    l++;
+                    r--;
                 }
             
                 if (sum>0) {
